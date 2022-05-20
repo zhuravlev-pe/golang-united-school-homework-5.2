260c9ef946750920634010355a9e2005fb5f03ce
zhuravlev_pe <zhuravlev.pe@gmail.com>
2022-05-20 08:30:01 +0700

    Implemented a solution

--- cache.go
+++ cache.go
@@ -2,22 +2,58 @@ package cache
 
 import "time"
 
+type item struct {
+	value      string
+	expiration time.Time
+}
+
+func (i *item) isExpired() bool {
+	if i.expiration.IsZero() {
+		return false
+	}
+	now := time.Now()
+	return i.expiration.Before(now)
+}
+
 type Cache struct {
+	items map[string]item
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{items: map[string]item{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	i, ok := c.items[key]
+	if !ok {
+		return "", false
+	}
+	if i.isExpired() {
+		delete(c.items, key)
+		return "", false
+	}
+	return i.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	i := item{value: value}
+	c.items[key] = i
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	result := make([]string, 0, len(c.items))
+
+	for k := range c.items {
+		_, ok := c.Get(k)
+		if ok {
+			result = append(result, k)
+		}
+	}
+
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	i := item{value: value, expiration: deadline}
+	c.items[key] = i
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhuravlev-pe/golang-united-school-homework-5.2
+
+go 1.17
